6c6c7087bfb11479d3582fd273674178d912b479
monger18 <satyamsri1809@gmail.com>
2024-08-03 19:01:58 +0530

    middleware and user route done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,67 +1,55 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
-import { sign } from 'hono/jwt'
-
+import { verify } from 'hono/jwt'
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string
     JWT_SECRET: string
   }
+  Variables: {
+    userId: string
+  }
 }>()
 
-app.post('/api/v1/signup', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
-  }).$extends(withAccelerate())
+/*******Middlewares */
 
-  const body = await c.req.json()
-  try {
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password,
-      },
-    })
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
-    return c.json({ jwt })
-  } catch (error) {
+app.use('/api/v1/blog/*', async (c, next) => {
+  const token = c.req.header('Authorization')
+  if (!token) {
     c.status(403)
-    return c.json({ error: 'Error while signing up' })
+    return c.json({ error: 'Unauthorized' })
   }
-})
-
-app.post('/api/v1/user/signin', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
-  }).$extends(withAccelerate())
-
-  const body = await c.req.json()
-  const user = await prisma.user.findUnique({
-    where: {
-      email: body.email,
-    },
-  })
-  if (!user) {
+  const jwt = token.split(' ')[1]
+  const payload = await verify(jwt, c.env.JWT_SECRET)
+  if (!payload) {
     c.status(403)
-    return c.json({ error: 'user not found' })
+    return c.json({ error: 'Unauthorized' })
   }
 
-  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
-
-  return c.json({ jwt })
+  c.set('userId', String(payload.id))
+  await next()
 })
+
+app.route('/api/v1/user', userRouter)
+app.route('/api/v1/blog', blogRouter)
+
 app.post('/api/v1/blog', (c) => {
+  console.log(c.get('userId'))
+
   return c.text('blog route')
 })
+
 app.put('/api/v1/blog', (c) => {
   return c.text('blog put route')
 })
+
 app.get('/api/v1/blog/:id', (c) => {
   const id = c.req.param('id')
   console.log(id)
   return c.text('blog get route')
 })
+
 app.get('/api/v1/blog/bulk', (c) => {
   return c.text('all blog route')
 })
